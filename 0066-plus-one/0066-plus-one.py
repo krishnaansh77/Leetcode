ffaf10ba866d63576d9f1b4a3de737896eed92a7
krishnaansh77 <nexusayush200477@gmail.com>
2026-08-05 22:09:51 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.99%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def plusOne(self, digits):
+        length=len(digits)
+        for i in range(length-1,-1,-1):
+            if digits[i]<9:
+                digits[i]=digits[i]+1
+                return digits
+            digits[i]=0
+        return [1]+digits
+        
